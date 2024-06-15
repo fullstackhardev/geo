2d978b5ce86013d765da283131aefdde4aeee18e
Hardev Singh <fullstackhardev@gmail.com>
2024-06-15 18:24:59 +0530

    1st step done

---
+++ database/migrations/2024_06_15_125257_create_geo_location_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('geo_location_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('ip_address');
+            $table->string('country');
+            $table->string('region');
+            $table->string('city');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('geo_location_logs');
+    }
+};
