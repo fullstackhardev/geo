fba4de60983924e8a72df80544f816aa474e30c6
Hardev Singh <fullstackhardev@gmail.com>
2024-06-15 23:48:32 +0530

    removed hidden class

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -393,7 +393,7 @@
 <body class="antialiased">
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        <div class="hidden fixed top-0 left-0 px-6 py-4 sm:block">
+        <div class=" fixed top-0 left-0 px-6 py-4 sm:block">
             <a href="{{ route('geo-location-log.index') }}"
                 class="text-sm text-gray-700 dark:text-gray-500 underline">Geo
                 Logs</a>
